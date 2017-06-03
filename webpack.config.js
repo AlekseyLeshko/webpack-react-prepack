65a27beb7dce108db25d334d1630bd35eb18f921
AlekseyLeshko <alekseyleshko@gmail.com>
2017-06-04 00:43:54 +0400

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
-module.exports = {
-  entry: path.join(__dirname, 'src'),
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+  entry: [
+    path.join(__dirname, './src/index.js')
+  ],
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist')
   },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          'es2015',
+          'react'
+        ]
+      }
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      root: __dirname,
+      verbose: true,
+      dry: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs',
+      inject: 'body',
+    })
+  ],
+  devServer: {
+    inline: true,
+    port: 3333,
+    contentBase: './dist',
+    noInfo: true
+  }
 };
 

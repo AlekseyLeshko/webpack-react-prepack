fd4372f61575b84f6531ee207d4eeef1f915429f
AlekseyLeshko <alekseyleshko@gmail.com>
2017-06-04 11:40:51 +0400

    add resolve in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -6,10 +7,25 @@ module.exports = {
   entry: [
     path.join(__dirname, './src/index.js')
   ],
+
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist')
   },
+
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src')
+    ],
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.css'
+    ],
+  },
+
   module: {
     loaders: [{
       test: /\.js$/,
@@ -23,6 +39,14 @@ module.exports = {
       }
     }]
   },
+
+  devServer: {
+    inline: true,
+    port: 3333,
+    contentBase: './dist',
+    noInfo: true
+  },
+
   plugins: [
     new CleanWebpackPlugin(['dist'], {
       root: __dirname,
@@ -32,13 +56,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.ejs',
       inject: 'body',
-    })
+    }),
+    new webpack.DefinePlugin({
+      __DEV__: true,
+    }),
   ],
-  devServer: {
-    inline: true,
-    port: 3333,
-    contentBase: './dist',
-    noInfo: true
-  }
 };
 

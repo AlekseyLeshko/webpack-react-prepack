005c586e4b7685ae99b9ae0e8e52cb758fa30720
AlekseyLeshko <alekseyleshko@gmail.com>
2017-06-04 11:41:15 +0400

    add example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-console.log('Hello world!');
+import service1 from 'services/service1.js';
+import temp from 'services/temp/index.js';
 
-// console.log(__DEV__);
-// if (__DEV__) {
-  // console.log('dev');
-// }
+console.log('Hello world!');
+console.log(service1);
+console.log(temp);
+
+console.log(__DEV__);
+if (__DEV__) {
+  console.log('dev');
+}
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import Game from './components/Game';
 
-ReactDOM.render(
-  <div>hello</div>,
+ReactDOM.render((
+  <div>
+    hello {JSON.stringify(service1)}
+    {JSON.stringify(temp)}
+  </div>),
   document.getElementById('root')
 );
 
---
+++ src/services/service1.js
@@ -0,0 +1,4 @@
+export default {
+  name: 'server 1',
+};
+

1105946a83f38fdf744c7a310c75d972c83000e9
AlekseyLeshko <alekseyleshko@gmail.com>
2017-11-11 17:45:55 +0400

    complicate example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import service1 from './services/service1.js';
+
 (function() {
   function fib(x) {
     return x <= 1 ? x : fib(x - 1) + fib(x - 2);
@@ -7,4 +9,8 @@
   if (x * 2 > 42) x = fib(10);
   global.result = x;
 })();
+
+console.log(service1);
+const a = JSON.stringify(service1);
+console.log(a);
 
